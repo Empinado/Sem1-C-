5ece1618512c2c1a9bac874a30fdc62ef51e14ca
Alexandr Degtyarev <Sasha20111997@gmail.com>
2022-11-16 13:14:33 +0500

    added task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -8,21 +8,33 @@ if (number == 1)
 if (number == 2)
 {
     Console.Write("Вторник");
-}if (number == 3)
+}
+if (number == 3)
 {
     Console.Write("Среда");
-}if (number == 4)
+}
+if (number == 4)
 {
     Console.Write("Четверг");
-}if (number == 5)
+}
+if (number == 5)
 {
     Console.Write("Пятница");
-}if (number == 6)
+}
+if (number == 6)
 {
     Console.Write("Суббота");
-}if (number == 7)
+}
+if (number == 7)
 {
     Console.Write("Воскресение");
 }
-
+if (number > 7)
+{
+    Console.Write("В неделе всего 7 дней, попробуйте снова.");
+}
+if (number < 1)
+{
+    Console.Write("В неделе всего 7 дней, попробуйте снова.");
+}    
 
