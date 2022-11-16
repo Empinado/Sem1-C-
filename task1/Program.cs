eb8a30e03ea967259da46d63442f43c67b15b35e
Alexandr Degtyarev <Sasha20111997@gmail.com>
2022-11-16 12:23:03 +0500

    added task1

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 / number2 == number2)
+{
+    Console.Write($"Да, число {number1} является корнем {number2}");
+}
+else
+{
+    Console.Write($"Нет, число {number1} не является корнем {number2}");
+}
